d80108c280b89b929a225713ce914a29dea29379
ZhouLan <1832311982@qq.com>
2019-09-15 12:21:57 +0800

    暑假最后几天

--- day_last/day_last/test.c
+++ day_last/day_last/test.c
@@ -4,25 +4,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//判断一个数是否是2^k
-int IsPowTow(int n){
-	for (int i = 1; i <= n; i <<= 1){
-		if (i == n) return 1;
-	}
-	return 0;
-}
-
-//二进制1的个数只有一个的话，就是2^k
-int IsPowTow2(int n){
-	int cnt = 0;
-	while (n){
-		n = n&(n - 1);
-		cnt++;
-	}
-	if (cnt == 1) return 1;
-	else return 0;
-}
-
 //删除字符串的空格 首尾删除 中间留一个空格 
 void DelBlank(char *str){
 	int i = 0;
@@ -52,11 +33,29 @@ void DelBlank(char *str){
 
 //字符串压缩
 
+//判断一个数是否是2^k
+int IsPowTow(int n){
+	for (int i = 1; i <= n; i <<= 1){
+		if (i == n) return 1;
+	}
+	return 0;
+}
+
+//二进制1的个数只有一个的话，就是2^k
+int IsPowTow2(int n){
+	int cnt = 0;
+	while (n){
+		n = n&(n - 1);
+		cnt++;
+	}
+	if (cnt == 1) return 1;
+	else return 0;
+}
 
 int main(){
-	printf("%d\n", IsPowTow(32));
+	printf("%d\n", IsPowTow(64));
 	printf("%d\n", IsPowTow(31));
-	printf("%d\n", IsPowTow2(32));
+	printf("%d\n", IsPowTow2(64));
 	printf("%d\n", IsPowTow2(31));
 	char string[] = "   asd  rf g       d  ";
 	DelBlank(string);
