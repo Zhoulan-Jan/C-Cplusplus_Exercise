040f9a6371d69803e43619f12a047c0c15d47e1f
ZhouLan <1832311982@qq.com>
2019-12-10 12:11:08 +0800

    函数模板

---
+++ 191210/191210/bubb.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<class T>
+void bubble(T * item, int len) {
+	int i = 0;
+	int j = 0;
+	T t;
+	for (; i < len - 1; i++) {
+		for (j = 0; j < len - i - 1; j++) {
+			if (item[j] > item[j + 1]) {
+				t = item[j];
+				item[j] = item[j + 1];
+				item[j + 1] = t;
+			}
+		}
+	}
+}
+
+int main() {
+	char str[] = "HUTREADHGKLP";
+	bubble(str, strlen(str));
+	cout << str << endl;
+
+	int nums1[] = { 7, 5, 1, 2, 3, 9, 6, 1 };
+	int len1 = sizeof(nums1) / sizeof(nums1[0]);
+	bubble(nums1, len1);
+	for (int i = 0; i < len1; i++) {
+		printf("%d ", nums1[i]);
+	}
+
+	double nums2[] = { 2.3, 5.3, 7.8, 3.9, 1.5 };
+	int len2 = sizeof(nums2) / sizeof(nums2[0]);
+	bubble(nums2, len2);
+	for (int i = 0; i < len2; i++) {
+		printf("%f ", nums2[i]);
+	}
+
+	system("pause");
+	return 0;
+}
