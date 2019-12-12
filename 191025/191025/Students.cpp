8db8fbd10044e043ed2cc800bb16e956a07f6cad
ZhouLan <1832311982@qq.com>
2019-12-12 10:22:26 +0800

    类与对象初始化

--- 191025/191025/Students.cpp
+++ 191025/191025/Students.cpp
@@ -1,58 +1,58 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#include<cstdio>
-#include<cstdlib>
-#include<iostream>
-#include<cmath>
-#include<string>
-using namespace std;
-
-//学生类
-class student {
-private:
-	string AdmissionNum;
-	string name;
-	int score;
-	static int teamScore;
-public:
-	void Init();
-	void Print();
-};
-
-//3名学生初始化
-void student::Init(student students[]) {
-	//student students[3];
-	students[0].AdmissionNum = "001";
-	students[0].name = "Peter";
-	students[0].score = 88;
-	/*teamScore = 85;*/
-
-	students[1].AdmissionNum = "002";
-	students[1].name = "Lora";
-	students[1].score = 99;
-
-	students[2].AdmissionNum = "003";
-	students[2].name = "Ron";
-	students[2].score = 79;
-}
-
-//信息输出
-void student::Print(student stu) {
-	for (int i = 0; i < 3; i++) {
-		cout << stu.AdmissionNum << endl;
-		cout << stu.name << endl;
-		cout << stu.score << endl;
-	}
-	cout << "成功！";
- }
-
-int main() {
-	student stu[3];
-	stu.Init(stu);
-	stu.Print(stu);
-	system("pause");
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<cstdio>
+//#include<cstdlib>
+//#include<iostream>
+//#include<cmath>
+//#include<string>
+//using namespace std;
+//
+////学生类
+//class student {
+//private:
+//	string AdmissionNum;
+//	string name;
+//	int score;
+//	static int teamScore;
+//public:
+//	void Init();
+//	void Print();
+//};
+//
+////3名学生初始化
+//void student::Init(student students[]) {
+//	//student students[3];
+//	students[0].AdmissionNum = "001";
+//	students[0].name = "Peter";
+//	students[0].score = 88;
+//	/*teamScore = 85;*/
+//
+//	students[1].AdmissionNum = "002";
+//	students[1].name = "Lora";
+//	students[1].score = 99;
+//
+//	students[2].AdmissionNum = "003";
+//	students[2].name = "Ron";
+//	students[2].score = 79;
+//}
+//
+////信息输出
+//void student::Print(student stu) {
+//	for (int i = 0; i < 3; i++) {
+//		cout << stu.AdmissionNum << endl;
+//		cout << stu.name << endl;
+//		cout << stu.score << endl;
+//	}
+//	cout << "成功！";
+// }
+//
+//int main() {
+//	student stu[3];
+//	stu.Init(stu);
+//	stu.Print(stu);
+//	system("pause");
+//	return 0;
+//}
 
 //#include<iostream>
 //using namespace std;
--- 191025/191025/test.cpp
+++ 191025/191025/test.cpp
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//#include<cstdio>
-//#include<cstdlib>
-//#include<iostream>
-//#include<cmath>
-//using namespace std;
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#include<cmath>
+using namespace std;
 //
-//
+//面向过程的方式书写
 ////圆形游泳池
 //#define pi 3.14
 //double *Price(int radius) {
@@ -24,3 +24,42 @@
 //	system("pause");
 //	return 0;
 //}
+
+//面向对象的方式书写
+class pool{
+public:
+	//得到半径长度
+	void getRadius();
+	//栅栏总价
+	double getFenceTotal();
+	//过道总价
+	double getAisleTotal();
+private:
+	int aisle = 3; // 过道宽度
+	int radius; //游泳池宽度
+	int fencePri = 35;//栅栏价格
+	int aislePri = 20;//过道造价
+};
+
+void pool::getRadius() {
+	cout<< "请输入游泳池的半径：";
+	cin >> radius;
+}
+
+double pool::getFenceTotal() {
+	return 2 * 3.14*(radius + aisle)*fencePri;
+}
+
+double pool::getAisleTotal() {
+	double x = (3.14*(radius + 3)*(radius + 3) - 3.14*(radius*radius));
+	return x*aislePri;
+}
+
+int main() {
+	pool p;
+	p.getRadius();
+	printf("过道的价格为：%f \n", p.getAisleTotal());
+	printf("栅栏的价格为：%f \n", p.getFenceTotal());
+	system("pause");
+	return 0;
+}
