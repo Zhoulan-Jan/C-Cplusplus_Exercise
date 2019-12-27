5bb62c57176ca9ead2a5fae0bb5782f459b3f627
ZhouLan <1832311982@qq.com>
2019-12-27 10:30:54 +0800

    使用随机数生成物品参数

--- 191127/191127/test.cpp
+++ 191127/191127/test.cpp
@@ -15,10 +15,10 @@ void Print(vector<vector<int>> x);
 //随机产生100个物品 size , value
 void BuildArr(int *size, int *value, int len) {
 	for (int i = 0; i < len; i++) {
-		size[i] = rand() % 100 + 1;
+		size[i] = rand() % 10 + 1;
 	}
 	for (int i = 0; i < len; i++) {
-		value[i] = rand() % 100 + 1;
+		value[i] = rand() % 10 + 1;
 	}
 }
 //生成背包容量 物品总量的0.6-0.7
@@ -37,47 +37,8 @@ int max(int x, int y) {
 int min(int x, int y) {
 	return x > y ? y : x;
 }
-//动态规划 书上代码 从下往上 从左往右
-//void Knapsack(int* v, int* w, int c, int n, int **m) {
-//	int jMax = min(w[n] - 1, c);
-//	for (int j = 0; j < jMax; j++) {
-//		m[n][j] = 0;
-//	}
-//	for (int j = w[n]; j < c; j++) {
-//		m[n][j] = v[n];
-//	}
-//	for (int i = n - 1; i > 0; i--) {
-//		jMax = min(w[i] - 1, c);
-//		for (int j = 0; j < jMax; j++) {
-//			m[i][j] = m[i + 1][j];
-//		}
-//		for (int j = w[i]; j < c; j++) {
-//			m[i][j] = max(m[i + 1][j], m[i + 1][j - w[i]] + v[i]);
-//		}
-//	}
-//	m[1][c] = m[2][c];
-//	if (c >= w[1]){
-//		m[1][c] = max(m[1][c], m[2][c - w[1]] + v[1]);
-//	}
-//}
-//
-//void Traceback(int **m, int* w, int c, int n, int* x){
-//	for (int i = 0; i < n; i++) {
-//		if (m[i][c] == m[i + 1][c]){
-//			x[i] = 0;
-//		}
-//		else {
-//			x[i] = 1;
-//			c -= w[i];
-//		}
-//		x[n] = (m[n][c]) ? 1 : 0;
-//	}
-//}
 
-
-
-//bit教的动态规划求背包
-//从上往下 从左往右
+//bit教的动态规划求背包 //从上往下 从左往右
 vector<vector<int>> Knapsack(int capacity, int* size, int* value, int n) {
 	vector <vector <int>> F;
 	const int ROW = n + 1;
@@ -85,7 +46,7 @@ vector<vector<int>> Knapsack(int capacity, int* size, int* value, int n) {
 	F.resize(n+1);
 	//初始化全为零
 	for (int i = 0; i < ROW; i++) {
-		F[i].resize(COL, 0);
+		F[i].resize(capacity + 1, 0);
 	}
 	//状态递推
 	for (int i = 1; i < ROW; i++) {  
@@ -125,9 +86,9 @@ void Traceback(vector<vector<int>> F, int num, int capacity, int *size) {
 }
 
 
-void Print(vector<vector<int>> x) {
-	for (int i = 0; i < x.size(); i++) {
-		for (int j = 0; j < x[i].size(); j++) {
+void Print(vector<vector<int>> x, int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
 			printf("%2d ", x[i][j]);
 		}
 		printf("\n");
@@ -143,46 +104,51 @@ int main(){
 	srand((unsigned)time(NULL));
 
 	//固定值
-	int num = 5; //物品数量
-	
-	int capacity = 12; //背包容量
-	//capacity = 10;
-	
-	int size[] = {1,3,14,5,2}; //物品重量
-	//int size[] = { 2, 2, 6, 5, 4 };
-	
-	int value[] = { 3,5,10,2,2}; //物品价值
-	//int value[] = { 6, 3, 5, 4, 6 };
+	//int num = 5; //物品数量
+	//
+	//int capacity = 12; //背包容量
+	////capacity = 10;
+	//
+	//int size[] = {1,3,14,5,2}; //物品重量
+	////int size[] = { 2, 2, 6, 5, 4 };
+	//
+	//int value[] = { 3,5,10,2,2}; //物品价值
+	////int value[] = { 6, 3, 5, 4, 6 };
 
 	//随机值
-	//int num = NUM;
-	//int size[NUM];
-	//int value[NUM];
-	//BuildArr(size, value, NUM);
-	//int capacity = getCap(size, NUM);
+	int num = NUM;
+	int size[NUM] = { 0 };
+	int value[NUM] = { 0 };
+	BuildArr(size, value, num);
+	int capacity = getCap(size, num);
 
-	//printf("%d %d ", num, capacity);
-	//for (int i = 0; i < NUM; i++){
-	//	printf("size: %d \n", size[i]);
-	//	printf("value: %d \n", value[i]);
-	//}
+	printf("物品数量：%d 背包容量：%d \n", num, capacity);
+	printf("物品体积：\n");
+	for (int i = 0; i < NUM; i++){
+		printf("size: %d ", size[i]);
+	}
+	
+	printf("\n物品价值：\n");
 
-	//m存储
-	int m[][10] = { 0 };
-	int x[5];
+	for (int i = 0; i < NUM; i++){
+		printf("valu: %d ", value[i]);
+	}
+
+	printf("\n");
 
 	vector<vector<int>> F;
-	F.resize(num + 1);
-	for (int i = 0; i < num+1; i++) {
-		F[i].resize(capacity+1, 0);
+	F.resize(10);
+	for (int i = 0; i < 10; i++) {
+		F[i].resize(10, 0);
 	}
 
 	//动态规划
 	F = Knapsack(capacity, size, value, num);
-	printf("状态递推二维数组\n");
-	Print(F);
+	printf("\n状态递推二维数组:\n");
+	Print(F,num + 1, capacity + 1);
+	printf("所选择的物品体积：\n");
 	Traceback(F, num, capacity, size);
-
+	printf("\n");
 	//回溯法
 
 	//分支限界法
