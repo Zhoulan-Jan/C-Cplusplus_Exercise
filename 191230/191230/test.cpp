fda7a6d784a15211c8b9b4440e4a67e4a93274fc
ZhouLan <1832311982@qq.com>
2019-12-30 23:16:21 +0800

    分支限界法求01背包

--- 191230/191230/test.cpp
+++ 191230/191230/test.cpp
@@ -7,6 +7,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<ctime>
 using namespace std;
 
 //子集树的结点
@@ -31,10 +32,15 @@ public:
 	}
 };
 
-const int num = 5; //物品数量
-int capacity = 10; //背包容量
-int size[] = { 2, 2, 6, 5, 4 }; //物品体积
-int value[] = { 6, 3, 5, 4, 6 }; //物品价值
+//const int num = 5; //物品数量
+//int capacity = 10; //背包容量
+//int size[] = { 2, 2, 6, 5, 4 }; //物品体积
+//int value[] = { 6, 3, 5, 4, 6 }; //物品价值
+
+const int num = 100; //物品数量
+int capacity; //背包容量
+int size[num]; //物品体积
+int value[num]; //物品价值
 
 int curSize = 0; //当前体积
 int curValue = 0; //当前价值
@@ -43,6 +49,25 @@ int best = 0; //当前最优价值
 bbnode *extend; //指向扩展结点的指针
 int* bestx; //最优解
 priority_queue<HeapNode<int, int>> H; //优先队列
+
+//随机产生100个物品 size , value
+void BuildArr(int *size, int *value, int len) {
+	for (int i = 0; i < len; i++) {
+		size[i] = rand() % 10 + 1;
+	}
+	for (int i = 0; i < len; i++) {
+		value[i] = rand() % 10 + 1;
+	}
+}
+
+//生成背包容量 物品总量的0.6-0.7
+int getCap(int *size, int len) {
+	int sum = 0;
+	for (int i = 0; i < len; i++) {
+		sum += size[i];
+	}
+	return sum * 0.6;
+}
 
 //简单版的限界函数
 int Bound(int i) {
@@ -80,7 +105,7 @@ int MaxKnapsack() {
 	int bestValue = 0; //当前最优值
 	int up = Bound(1); //计算上界
 	//搜索子集空间树
-	while (i != num + 1) {
+	while (i != num) {
 		if (curSize + size[i] <= capacity) {
 			if (curValue + value[i] > bestValue) {
 				bestValue = curValue + value[i];
@@ -101,16 +126,37 @@ int MaxKnapsack() {
 		i = N.level;
 	}
 	//构造当前最优解
-	//for (int j = num; j > 0; j++) {
-	//	//bestx[j] = extend->LChild;
-	//	//extend = extend->parent;
-	//	printf("%d ", bestx[j]);
-	//}
+	for (int j = num - 1; j >= 0; j--) {
+		bestx[j] = extend->LChild;
+		extend = extend->parent;
+		//printf("%d ", extend->LChild);
+		//extend = extend->parent;
+	}
 	return curValue;
 }
 
 int main() {
-	printf("%d ", MaxKnapsack());
+	srand((unsigned)time(NULL));
+	BuildArr(size, value, num);
+	capacity = getCap(size, num);
+	clock_t run_start = clock();
+	printf("最大价值为：%d \n", MaxKnapsack());
+	clock_t run_end = clock();
+	double runtime = ((double)run_end - (double)run_start) / CLOCKS_PER_SEC;
+	printf("Run Time: %.3f s \n", runtime);
+
+	printf(" 物品的体积为：");
+	for (int i = 0; i < num; i++) {
+		printf("%d ", size[i]);
+	}
+	printf("\n 物品的价值为：");
+	for (int i = 0; i < num; i++) {
+		printf("%d ", value[i]);
+	}
+	printf("\n1表示放该物品：");
+	for (int i = 0; i < num; i++) {
+		printf("%d ", bestx[i]);
+	}
 	system("pause");
 	return 0;
 }
